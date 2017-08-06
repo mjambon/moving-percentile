6f4231733d8d4b5340717f45b3b05ac0626812fe
Martin Jambon <martin@mjambon.com>
2017-08-06 02:00:51 -0700

    Another failed experiment

--- mv_adapt.ml
+++ mv_adapt.ml
@@ -9,79 +9,44 @@ open Printf
 
 type state = {
   alpha_avg: float;
-  alpha_slope: float;
-
-  alpha_avg_half_weight: float;
-  alpha_slope_half_weight: float;
+  alpha_missing: float;
 
   avg: Mv_avg.state;
-  slope: Mv_avg.state;
+  missing: Mv_avg.state;
 
-  mutable last_sample: float;
   mutable smoothed: float;
 }
 
-(*
-let default_alpha_avg = 0.0561257 (* half-weight = 12 samples *)
-let default_alpha_slope = 0.1091013 (* half-weight = 6 samples *)
-*)
-let default_alpha_avg = 0.10
-let default_alpha_slope = 0.05
-
-(*
-   Determine number of samples with a weight of half in an exponential
-   moving average of parameter alpha.
-
-   alpha = 0.0561257 -> 12 samples
-   alpha = 0.1091013 -> 6 samples
-*)
-let get_half_weight alpha =
-  let result = ref 0 in
-  let sum = ref 0. in
-  try
-    for i = 0 to 1000 do
-      sum := !sum +. alpha *. (1. -. alpha) ** (float i);
-      (* printf "[%i] %g\n" i !sum; *)
-      if !sum >= 0.5 then (
-        result := i;
-        raise Exit
-      )
-    done;
-    assert false
-  with Exit ->
-    !result + 1
+let default_alpha_avg = 0.05
+let default_alpha_missing = 0.5
 
 let init
   ?(alpha_avg = default_alpha_avg)
-  ?(alpha_slope = default_alpha_slope)
+  ?(alpha_missing = default_alpha_missing)
   () =
 
   {
     alpha_avg;
-    alpha_slope;
-    alpha_avg_half_weight = float (get_half_weight alpha_avg);
-    alpha_slope_half_weight = float (get_half_weight alpha_slope);
+    alpha_missing;
     avg = Mv_avg.init ~alpha:alpha_avg ();
-    slope = Mv_avg.init ~alpha:alpha_slope ();
-    last_sample = nan;
+    missing = Mv_avg.init ~alpha:alpha_missing ();
     smoothed = nan;
   }
 
 let update state x =
   assert (x = x);
   let diff =
-    let last = state.last_sample in
+    let last = Mv_avg.get state.avg in
     if last = last then
       x -. last
     else
       0.
   in
-  Mv_avg.update state.slope diff;
   Mv_avg.update state.avg x;
-  state.last_sample <- x;
+  Mv_avg.update state.missing diff;
   let avg = Mv_avg.get state.avg in
-  let slope = Mv_avg.get state.slope in
-  let smoothed = avg +. slope *. state.alpha_avg_half_weight in
+  let missing = Mv_avg.get state.missing in
+  let smoothed = avg +. missing in
   state.smoothed <- smoothed
 
 let get_smoothed state =
