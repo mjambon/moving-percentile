f481db12314e07a3b42c7f10ec040110562ca65b
Martin Jambon <martin@mjambon.com>
2017-08-06 13:59:55 -0700

    Simplify csv output

--- adapt_main.ml
+++ adapt_main.ml
@@ -21,37 +21,32 @@ let add_noise x = x +. Random.float 4.
 let input_with_noise = List.map add_noise input_no_noise
 
 let print_csv_header () =
-  printf "i,x,mean_long,mean_short,\
-          mv_exp_long,mv_exp_short,mv_exp_best,\
-          adapt_avg,gain,loss,alpha\n"
+  printf "i,x,\
+          adapt_avg,\
+          mv_exp_best,\
+          mv_mean_best,\
+          gain,loss,alpha\n"
 
 let print_csv_row
-    ~i ~x ~mv_mean_long ~mv_mean_short
-    ~mv_exp_long ~mv_exp_short ~mv_exp_best
-    ~adapt_avg ~gain ~loss ~alpha =
-  printf "%i,%g,%g,%g,%g,%g,%g,%g,%g,%g,%g\n"
-    i x mv_mean_long mv_mean_short mv_exp_long mv_exp_short mv_exp_best
-    adapt_avg gain loss alpha
+    ~i ~x
+    ~adapt_avg
+    ~mv_exp_best
+    ~mv_mean_best
+    ~gain ~loss ~alpha =
+  printf "%i,%g,%g,%g,%g,%g,%g,%g\n"
+    i x
+    adapt_avg
+    mv_exp_best
+    mv_mean_best
+    gain loss alpha
 
 let test_series input =
-  let state_mean_long = Mv_naive_mean.init ~window_length:30 in
-  let state_mean_short = Mv_naive_mean.init ~window_length:3 in
-  let state_mv_exp_long = Mv_avg.init ~alpha:Mv_adapt.default_alpha_min () in
-  let state_mv_exp_short = Mv_avg.init ~alpha:Mv_adapt.default_alpha_max () in
-  let state_mv_exp_best = Mv_avg.init ~alpha:0.3 () in
   let state = Mv_adapt_avg.init () in
+  let state_mv_exp_best = Mv_avg.init ~alpha:0.3 () in
+  let state_mv_mean_best = Mv_naive_mean.init ~window_length:8 in
   let process_sample i x =
-    Mv_naive_mean.update state_mean_long x;
-    let mv_mean_long = Mv_naive_mean.get state_mean_long in
-
-    Mv_naive_mean.update state_mean_short x;
-    let mv_mean_short = Mv_naive_mean.get state_mean_short in
-
-    Mv_avg.update state_mv_exp_long x;
-    let mv_exp_long = Mv_avg.get state_mv_exp_long in
-
-    Mv_avg.update state_mv_exp_short x;
-    let mv_exp_short = Mv_avg.get state_mv_exp_short in
+    Mv_naive_mean.update state_mv_mean_best x;
+    let mv_mean_best = Mv_naive_mean.get state_mv_mean_best in
 
     Mv_avg.update state_mv_exp_best x;
     let mv_exp_best = Mv_avg.get state_mv_exp_best in
@@ -64,9 +59,11 @@ let test_series input =
     let adapt_avg = get state in
 
     print_csv_row
-      ~i ~x ~mv_mean_long ~mv_mean_short
-      ~mv_exp_long ~mv_exp_short ~mv_exp_best
-      ~adapt_avg ~gain ~loss ~alpha
+      ~i ~x
+      ~adapt_avg
+      ~mv_exp_best
+      ~mv_mean_best
+      ~gain ~loss ~alpha
   in
   print_csv_header ();
   Array.iteri process_sample (Array.of_list input)
